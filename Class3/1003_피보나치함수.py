dc38480f5973af687be8b71c044ace37d12e2ae3
WoongJeong <zzamdoly@gmail.com>
2023-07-22 12:21:35 +0900

    add 1003 피보나치함수

--- Class3/1003_피보나치함수.py
+++ Class3/1003_피보나치함수.py
@@ -1,17 +1,7 @@
 import sys
-from collections import deque, Counter
+from collections import deque
 
-def fibonacci(counter: list, n: int) -> int:
-    if n == 0:
-        counter[0] += 1
-        return 0
-    elif n == 1:
-        counter[1] += 1
-        return 1
-    else:
-        return fibonacci(counter, n-1) + fibonacci(counter, n-2)
-
-def fibonacci_memoization(n: int):
+def fibonacci_memoization(n: int) -> deque:
     fibo_deque = deque(
         [
             {0:1, 1:0},
@@ -33,9 +23,5 @@ T = int(sys.stdin.readline().strip())
 for _ in range(T):
     N = int(sys.stdin.readline().strip())
     
-    #fin_result = [0, 0]
-    #fibonacci(fin_result, N)
-    #print(fin_result[0], fin_result[1])
-
     result = fibonacci_memoization(N)
-    print(result[-1][0], result[-1][1])
+    print(result[N][0], result[N][1])
