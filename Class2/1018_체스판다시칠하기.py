22701f4d1c8c8794c6a647df76cf30c420a0fb99
WoongJeong <zzamdoly@gmail.com>
2023-08-01 00:58:15 +0900

    1018 체스판다시칠하기 오답

--- Class2/1018_체스판다시칠하기.py
+++ Class2/1018_체스판다시칠하기.py
@@ -1,40 +1,52 @@
 import sys
-from collections import deque
 
-N, M = map(int, sys.stdin.readline().strip().split())
-search = (range(N-8+2), range(M-8+2))
-print(search)
+def re_paint(in_array: list, x: int, y: int) -> None:
+    global count
+    if in_array[x][y] == "W":
+        in_array[x][y] = "B"
+    elif in_array[x][y] == "B":
+        in_array[x][y] = "W"
+    count += 1
+    
+def check_color(in_array: list, x: int, y: int) -> None:
+    if in_array[x][y] == in_array[x][y-1]:
+        re_paint(in_array, x, y)
 
+def search(in_array: list) -> None:
+    ini_color = in_array[0][0]
 
-orig_board = list()
+    for i in range(len(in_array)):
+        if i == 0:
+            ini_color = in_array[i][0]
+        else:                
+            if ini_color == in_array[i][0]:
+                re_paint(in_array, i, 0)
+            ini_color = in_array[i][0]
+        for j in range(1,len(in_array[i])):
+            check_color(in_array, i, j)
 
-for _ in range(N):
-    row = sys.stdin.readline().strip()
-    orig_board.append(list(row))
+if __name__ == '__main__':
 
-for i in search[0]:
-    for j in search[1]:
-        chess_board = []
-        for x in range(i,i+8+1):
-            chess_board.append(orig_board[i][j:j+8])
-        
-        print(chess_board)
+    N, M = map(int, sys.stdin.readline().strip().split())
+    chess_range = (range(N-8+1), range(M-8+1))
 
+    orig_board = list()
 
+    for _ in range(N):
+        row = sys.stdin.readline().strip()
+        orig_board.append(list(row))
 
-def re_paint(x: int, y: int) -> int:
-    changed = 0
-    try:
-        board[x-1][y]
-    except: pass
-    try:
-        board[x+1][y]
-    except: pass
-    try: 
-        board[x][y-1]
-    except: pass
-    try: 
-        board[x][y+1]
-    except: pass
+    counts = []
 
-    return changed
+    for i in chess_range[0]:
+        for j in chess_range[1]:
+
+            chess_board = []
+            for x in range(i, i+8):
+                chess_board.append(orig_board[x][j:j+8])            
+            
+            count = 0
+            search(chess_board)
+            counts.append(count)
+
+    print(min(counts))
